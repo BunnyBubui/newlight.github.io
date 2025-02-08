4516fc73fcbd4008b6b813e3e553d21b9572214b
Banyapon Chaithong <mainbanyapon@gmail.com>
2025-02-08 11:22:47 +0700

    up

--- js/login_needed.js
+++ js/login_needed.js
@@ -45,3 +45,71 @@
   window.onload = function() {
     document.getElementById('loader').style.display = 'none';
 };
+particlesJS("particles-js", {
+  particles: {
+      number: {
+          value: 100,
+          density: {
+              enable: true,
+              value_area: 800
+          }
+      },
+      color: {
+          value: "#ffffff" 
+      },
+      shape: {
+          type: "circle",
+          stroke: {
+              width: 0,
+              color: "#000000"
+          }
+      },
+      opacity: {
+          value: 0.5,
+          random: true,
+          anim: {
+              enable: true,
+              speed: 1,
+              opacity_min: 0.1,
+              sync: false
+          }
+      },
+      size: {
+          value: 3,
+          random: true,
+          anim: {
+              enable: true,
+              speed: 4,
+              size_min: 0.1,
+              sync: false
+          }
+      },
+      move: {
+          enable: true,
+          speed: 2,
+          direction: "none",
+          random: false,
+          straight: false,
+          out_mode: "out",
+          bounce: false,
+          attract: {
+              enable: false,
+              rotateX: 600,
+              rotateY: 1200
+          }
+      }
+  },
+  interactivity: {
+      detect_on: "canvas",
+      events: {
+          onhover: {
+              enable: true,
+              mode: "repulse"
+          },
+          onclick: {
+              enable: true,
+              mode: "push"
+          }
+      }
+  }
+});
